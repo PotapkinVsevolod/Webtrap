f74484c665ca74dd4c9be3b77556575f7ca83c21
potapkinvsevolod <depas123e26>
2021-03-08 19:11:47 +0700

    Добавил логи в .gitignore. Добавил логирование запросов к серверу.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import logging
+import time
+
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
+log = logging.getLogger('WebTrapLogger')
+handler = logging.FileHandler("main.log")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+formatter.converter = time.gmtime
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.INFO)
+
 
 @app.route('/<path:path>', methods=["DELETE", "GET", "PATCH", "POST", "PUT", "TRACE"])
 def query(path):
+    log.info(f'{request.method} - {request.path} - {request.args.to_dict()}')
     return 'OK', 200
 
 
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,13 +1,15 @@
 import unittest
-import datetime
+from datetime import datetime, timedelta
+
 from app import app
 
 
 class TestNotGetMethods(unittest.TestCase):
     def test_request_make_logs_entry(self) -> None:
         self.client = app.test_client()
-        self.url = 'test/url/1/1.2'
-        self.params = '?test1=test1&test2=test2'
+        self.path = 'test/url/1/1.2'
+        self.params = 'first_param=first_value&second_param=second_value'
+        self.url = f'{self.path}?{self.params}'
 
         for self.method, self.request in [
             ("GET", self.client.get),
@@ -17,13 +19,18 @@ class TestNotGetMethods(unittest.TestCase):
             ("PUT", self.client.put),
             ("TRACE", self.client.trace),
         ]:
-            now_time = datetime.datetime.utcnow()
-            self.request(self.url + self.params)
+
+            now_time = datetime.utcnow()
+            now_time = now_time - timedelta(microseconds=now_time.microsecond)
+
+            self.request(self.url)
+
             with open('main.log') as log:
                 last_entry = log.readlines()[-1]
-                self.assertIn(self.url, last_entry)
+                self.assertIn(self.path, last_entry)
                 self.assertIn(self.method, last_entry)
-                self.assertIn('test1=test1, test2=test2', last_entry)
-                timestamp = datetime.datetime.strptime(last_entry[:26], '%Y-%m-%d %H:%M:%S.%f')
-                self.assertGreater(timestamp, now_time)
-                self.assertLess(timestamp, now_time + datetime.timedelta(seconds=1))
+                self.assertIn("{'first_param': 'first_value', 'second_param': 'second_value'}", last_entry)
+
+                timestamp = datetime.strptime(last_entry[:19], '%Y-%m-%d %H:%M:%S')
+                self.assertGreaterEqual(timestamp, now_time)
+                self.assertLess(timestamp, now_time + timedelta(seconds=1))
